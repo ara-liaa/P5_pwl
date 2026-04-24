852d3fdb39d732800df7245454cae010e6746ee3
ara-liaa <nuralia1418@gmail.com>
2026-04-24 13:33:02 +0700

    Create Proses_login.php

---
+++ Proses_login.php
@@ -0,0 +1,12 @@
+<?php
+// proses_login.php
+
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+echo "Halo: $user";
+
+// URL tetap bersih:
+// localhost/proses...
+// Data TIDAK di URL
+?>
