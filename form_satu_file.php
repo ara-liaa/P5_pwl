2bcea6c6fbe0862b45e95f4f21457e1d30493463
ara-liaa <nuralia1418@gmail.com>
2026-04-24 14:13:14 +0700

    Update form_satu_file.php

--- form_satu_file.php
+++ form_satu_file.php
@@ -1,1 +1,26 @@
+<?php
+// form_satu_file.php — ACTION="" (self)
+if (isset($_REQUEST['nama'])) {
+    $nama = $_REQUEST['nama'];
+    $umur = $_REQUEST['umur'];
+    echo "Halo $nama, umur $umur";
+} else {
+    echo "Form belum diisi";
+}
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Satu File</title>
+</head>
+<body>
+
+<form action="" method="post">
+    Nama: <input name="nama"><br>
+    Umur: <input name="umur"><br>
+    <input type="submit" value="Kirim">
+</form>
+
+</body>
+</html>
